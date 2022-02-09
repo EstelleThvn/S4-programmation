0cd604d8d02dadab5d6199fd4e6b234dbfd0f6a7
EstelleThvn <estelle.thouvenin00@gmail.com>
2022-02-09 17:41:56 +0100

    [Use a dedbugger] Add a breakpoint in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,4 +6,9 @@ int main()
 {
     std::cout << "Hello World\n";
     plop();
+
+    int number_of_lives = 3;
+
+    //F9 to place a breakpoint
+    std::cout << "You have " << number_of_lives << " lives\n";
 }
